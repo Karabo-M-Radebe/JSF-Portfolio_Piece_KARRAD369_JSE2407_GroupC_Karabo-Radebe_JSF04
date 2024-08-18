a3804dc0f29c72516c4b5456874a3ef37de2c5de
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-08-19 01:25:12 +0200

    fetched the fakestore login api for user authorisation, then made the username and password use a ref because its a one-time use

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,26 @@
 <script setup>
+  import { ref } from 'vue'
+  
+  const username = ref('')
+  const password = ref('')
+  
+  function login() {
+    fetch('https://fakestoreapi.com/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+      }),
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.error('Error:', err))
+  }
+  </script>
 
-</script>
-
-<template>
- <h1>Login</h1>
+<template> 
+    <h1>Login page</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>  
